a8b2062a9bf04862944689e97b5bf40e673eb419
Evans <evansachie01@gmail.com>
2023-02-18 00:50:39 -0800

    alx

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,9 +14,9 @@ int main(void)
 		{
 			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
-				putchar((num1 + '0');
-				putchar((num2 + '0');
-				putchar((num3 + '0');
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
 
 				if (num1 != 7 || num2 != 8 || num3 != 9)
 				{
