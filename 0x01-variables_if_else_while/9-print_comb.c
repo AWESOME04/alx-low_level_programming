31111255488ab2dc4dda1edff03ddc769f0e9346
Evans <evansachie01@gmail.com>
2023-02-18 00:14:14 -0800

    alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,22 +13,28 @@
  * You are not allowed to use any variable of type char
  * Return: Always 0 (success)
  */
-int main(void)
-{
-	int num;
-	char la;
+int main() {
+    int i, j, k, l;
 
-	for (num = 0; num < 10; num++)
-	{
-		putchar((num % 10) + '0');
-	}
+    for (i = 0; i < 10; i++) {
+        for (j = i + 1; j < 10; j++) {
+            for (k = j + 1; k < 10; k++) {
+                for (l = k + 1; l < 10; l++) {
+                    putchar(i + '0');
+                    putchar(',');
+                    putchar(' ');
+                    putchar(j + '0');
+                    putchar(',');
+                    putchar(' ');
+                    putchar(k + '0');
+                    putchar(',');
+                    putchar(' ');
+                    putchar(l + '0');
+                    putchar('\n');
+                }
+            }
+        }
+    }
 
-	for (la = 'a'; la <= 'f'; la++)
-	{
-		putchar(la);
-	}
-
-	putchar('\n');
-
-	return (0);
+    return 0;
 }
